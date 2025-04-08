2acc98e303027117c9170f1e25e11281ee329ed7
Niloofarhp <niloofar.hp@gmail.com>
2025-04-08 19:11:28 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv(override=True)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "your-default-key")
 MODEL_NAME = "gpt-4o-mini"
 DB_NAME = "vector_db"
-DOCS_PATH = "/Users/niloofar/Documents/Projects/llm_engineering/Intelligent-Knowledge-Retrieval-System/Dataset/"
+DOCS_PATH = "Intelligent-Knowledge-Retrieval-System/Dataset/"
